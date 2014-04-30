7ed4a37657ecf523ec3bb6bfa163c7748d7398dd
sisiz <zimmerman.sisi@gmail.com>
2014-04-30 17:20:50 -0400

    Update MunchyCritterRunner.java

--- MunchyCritterRunner.java
+++ MunchyCritterRunner.java
@@ -6,10 +6,22 @@ public class MunchyCritterRunner(){
                 ActorWorld world = new ActorWorld();
 
                 MunckyCritter alice = new MunchyCritter();
+                Rock r1 = new Rock()
 
                 alice.setColor(Color.ORANGE);
 
+
+        //Test 1
                 world.add(new Location(7,8), alice);
+
+                world.add(new Location(6,9), r1);
+                world.add(new Location(3,8), r1);
+                world.add(new Location(1,2), r1);
+                world.add(new Location(5,6), r1);
+                world.add(new Location(8,1), r1);
+
+
+
 
                 world.show();
 
