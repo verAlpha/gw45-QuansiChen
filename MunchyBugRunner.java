e52f135a154878cb7e5c965a1904bcff05128cfc
sisiz <zimmerman.sisi@gmail.com>
2014-04-30 15:48:28 -0400

    Update MunchyBugRunner.java

--- MunchyBugRunner.java
+++ MunchyBugRunner.java
@@ -3,18 +3,18 @@ public class MunchyCritterRunner(){
 
         public static void main(String[] args){
 
-        ActorWorld world = new ActorWorld();
+                ActorWorld world = new ActorWorld();
 
-        BoxBug alice = new BoxBug(6);
+                BoxBug alice = new BoxBug(6);
 
-        alice.setColor(Color.ORANGE);
+                alice.setColor(Color.ORANGE);
 
-        BoxBug bob = new BoxBug(3);
+                BoxBug bob = new BoxBug(3);
 
-        world.add(new Location(7,8), alice);
+                world.add(new Location(7,8), alice);
 
-        world.add(new Location(5,5), bob);
+                world.add(new Location(5,5), bob);
 
-        world.show();
+                world.show();
 
 }
