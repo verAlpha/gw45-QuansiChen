a9c3e0ca527e6d1b2024b6bdb44f0d6d39ad82e2
sisiz <zimmerman.sisi@gmail.com>
2014-04-30 15:51:12 -0400

    Update and rename file1 to MunchyCritter.java

---
+++ MunchyCritter.java
@@ -0,0 +1,14 @@
+public class MunchyCritter extends Critter{
+
+        private void processActors(ArrayList<Actors> actors){
+                for( Actor a: actors){
+                        a.removeSelfFromGrid();
+                }
+                        
+        
+        
+        
+        }
+
+
+}
